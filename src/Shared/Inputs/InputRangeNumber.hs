755d5171d40aff6fd8bddd6b6d17b52c74f4c5db
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2025-01-27 00:04:37 +0700

    fix update range input validation

--- src/Shared/Inputs/InputRangeNumber.hs
+++ src/Shared/Inputs/InputRangeNumber.hs
@@ -2,17 +2,17 @@ module Shared.Inputs.InputRangeNumber (inputRangeNumber) where
 
 import System.Process (callCommand)
 import Shared.Validators.IsNumber
+import Text.Read (readMaybe)
 import Shared.Logs.Console
 
 inputRangeNumber :: String -> String -> Int -> Int -> IO Int
 inputRangeNumber infoMessage inputMessage minVal maxVal = do
-    putStrLn $ infoMessage
+    putStrLn infoMessage
     consoleInfo inputMessage
     input <- getLine
-    case isNumber input && read input >= minVal && read input <= maxVal
-        of
-            True -> return (read input)
-            False -> do
-                callCommand "cls"
-                consoleError ("Ошибка: введите число в диапазоне от " ++ (show minVal) ++ " до " ++ (show maxVal) ++ ".")
-                inputRangeNumber infoMessage inputMessage minVal maxVal
+    case readMaybe input of
+        Just value | value >= minVal && value <= maxVal -> return value
+        _ -> do
+            callCommand "cls"
+            consoleError ("Ошибка: введите число в диапазоне от " ++ show minVal ++ " до " ++ show maxVal ++ ".")
+            inputRangeNumber infoMessage inputMessage minVal maxVal
