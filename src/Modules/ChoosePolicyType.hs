4adb46b97595da429dd3000f122d1ad46945f13a
vladwebdev1402 <mmovlad.kriven.1402@gmail.com>
2025-01-28 01:57:11 +0700

    feat: add exit in policy choose menu

--- src/Modules/ChoosePolicyType.hs
+++ src/Modules/ChoosePolicyType.hs
@@ -7,5 +7,7 @@ import Shared.Logs.LogData (generateLogData)
 choosePolicyType :: IO (PolicyType)
 choosePolicyType = do
   policyTypes <- getPolicyTypes
-  index <- chooseData policyTypes (\array -> generateLogData array Entities.PolicyTypes.name) "\nВыберите вид страхования:" ""
-  return $ policyTypes !! (index - 1)
+  index <- chooseData policyTypes (\array -> generateLogData array Entities.PolicyTypes.name) "Введите 'выход', чтобы вернуться в меню\nВыберите вид страхования:" ""
+  case index of
+    -1 -> return PolicyType {uid = -1, name = ""}
+    _ -> return $ policyTypes !! (index - 1)
